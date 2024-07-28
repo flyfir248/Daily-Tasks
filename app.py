edd9dbbdc2bc5ad4694b72fd84303730dab4b9da
anoop <anoopjohny2000@gmail.com>
2024-07-28 19:10:34 +0530

    push : update import os module

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
